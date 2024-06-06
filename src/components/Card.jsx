dd91cabcfce35a16edf9125ebda04012e5a192d1
Samuel <samuferfort@gmail.com>
2024-06-06 16:03:40 +0200

    add propTypes to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
-export default function Card({ name, handleClick, sprite }) {
-    return (
-      <button key={name} className="card" name={name} onClick={handleClick}>
-        <img src={sprite} alt={name} />
-        <p>{name}</p>
-      </button>
-    );
-  }
-  
+import PropTypes from "prop-types";
+
+Card.propTypes = {
+  name: PropTypes.string,
+  handleClick: PropTypes.func,
+  sprite: PropTypes.string,
+};
+
+function Card({ name, handleClick, sprite }) {
+  return (
+    <button key={name} className="card" name={name} onClick={handleClick}>
+      <img src={sprite} alt={name} />
+      <p>{name}</p>
+    </button>
+  );
+}
+
+export default Card;
